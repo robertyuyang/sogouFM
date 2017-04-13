0e3825bba1841be5bcba49bd2bef4b81feb6ccab
Robert <robert@Roberts-MacBook-Pro.local>
2017-04-13 21:17:42 +0800

    Initial Commit

---
+++ sogouFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sogouFM
+//
+//  Created by Robert on 13/04/2017.
+//  Copyright © 2017 Sogou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
